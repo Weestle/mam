041e2f10dcb89075c8256181ea671194858e7c28
LASOV <44479103+Weestle@users.noreply.github.com>
2022-07-31 17:36:16 +0300

    123

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const screenWidtg = document.documentElement.clientWidth;
+const screenHeigth = document.documentElement.clientHeight;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(screenHeigth)
+console.log(screenWidtg)
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+      stripBg: "white",
+    };
+  }
+  render() {
+    const style = { backgroundColor: this.state.stripBg };
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          if (this.state.stripBg === "white") {
+            this.setState({ stripBg: "black" });
+          } else {
+            this.setState({ stripBg: "white" });
+          }
+        }}
+        style={style}
+      ></button>
+    );
+  }
+}
+
+class Field extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]} />;
+  }
+
+  render() {
+    let zxc = [[]];
+    let idx = 0;
+    const grid = [];
+    for (let i = 0; i < screenHeigth / 400; i++) {
+      const array_i = [];
+      for (let j = 0; j < screenWidtg / 400 - 1; j++) {
+        array_i.push(<Square value={this.state.squares[i]} />);
+      }
+      grid.push(array_i);
+    }
+    return (
+      <div className="grid">
+        {grid.map((row, rowID) => {
+          return row.map((node, nodeID) => {
+            return <Square className="square" value={this.state.squares[1]} />;
+          });
+        })}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="display">
+        <Field />
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
